a4312957b07cf8724af4b8d7e0fff941e61b8841
foodev <foodev@naver.com>
2021-06-19 22:49:14 +0900

    page done

--- src/logged_out/components/home/ListSection.js
+++ src/logged_out/components/home/ListSection.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
+
+
 import InboxIcon from '@material-ui/icons/Inbox';
 import DraftsIcon from '@material-ui/icons/Drafts';
+
+import Pagination from '@material-ui/lab/Pagination';
+
+import axios from 'axios';
+import { Fragment } from 'react';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,28 +23,74 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const useStylesPage = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
 
 
 
 export default function SimpleList() {
   const classes = useStyles();
+  const classesPage = useStylesPage();
+
+  const [totalCnt, setTotalCnt] =useState(0);
+
+
+  useEffect(()=>{
+    test();
+    
+  },[]);
+
+  const getCampingList = useCallback(async (page) => {
+    const datas = {};
+    datas['pageNumber'] = page;
+    datas['visiblePages'] = 5;
+    datas['showingContentNum'] = 5;
+    console.log(datas);
+    const res =await axios.post("http://localhost:3000/campingInfo/campInfoList",  datas)
+    console.log(res);
+  }, []);
+
+  const test = async () => {
+    const res =await axios.post("http://localhost:3000/campingInfo/campSpotAllcount")
+    console.log(res);
+    setTotalCnt(res.data.data.totalRow);
+  }
 
   return (
-    <div className={classes.root}>
-      <List component="nav" aria-label="main mailbox folders">
-        <ListItem button>
-          <ListItemIcon>
-            <InboxIcon />
-          </ListItemIcon>
-          <ListItemText primary="Inbox" />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <DraftsIcon />
-          </ListItemIcon>
-          <ListItemText primary="Drafts" />
-        </ListItem>
-      </List>
-    </div>
+    <Fragment>
+      <div className={classes.root}>
+        <List component="nav" aria-label="main mailbox folders">
+          <ListItem button>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <DraftsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Drafts" />
+          </ListItem>
+        </List>
+      </div>
+
+      <div className={classesPage.root}>
+      <Pagination count={totalCnt} color="primary" onChange={
+        (obj, page) => {
+          getCampingList(page);
+        }
+      } />
+      </div>
+    </Fragment>
+   
+
+
   );
 }
